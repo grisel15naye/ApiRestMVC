4eb00f581264d91f2cb4f4e2e5e969205c870ced
ramirezventuranayeli@gmail.com <ramirezventuranayeli@gmail.com>
2025-06-05 08:02:23 -0500

    inicioazion

---
+++ src/main/java/pe/idat/Secion4Exam/Secion4ExamApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.Secion4Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secion4ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secion4ExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/Secion4Exam/Secion4ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.Secion4Exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secion4ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
